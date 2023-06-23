39251791b8c08c36ade73906b657d8ca07f9682a
Nhi Le <michellelele156@gmail.com>
2023-06-23 15:05:45 -0400

    Initial Commit

---
+++ aboutme/aboutmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  aboutmeApp.swift
+//  aboutme
+//
+//  Created by Scholar on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct aboutmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
